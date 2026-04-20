b5e53f7ebb4bfca5319f185399736e615f9dce42
Edson <edsonj82@outlook.com>
2026-04-20 18:41:50 -0300

    extend player test with song playback interaction

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -6,4 +6,8 @@ test('it should play a song', async ({ page }) => {
   // Expect a title "to contain" a substring.
   const loggedUser = page.locator('.logged-user');
   await expect(loggedUser).toHaveText('Fernando Papito');
+
+  await page.click('//div[contains(@class,"song")]//h6[text()="Bughium"]/..//button')
+
+  await page.waitForTimeout(5000);
 });
