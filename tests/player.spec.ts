52d13dd48d28982b3bae4ebb95c1c5a9e805494b
Edson <edsonj82@outlook.com>
2026-05-02 14:56:35 -0300

    test: implement interaction test for music song button

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -3,8 +3,11 @@ import { test, expect } from '@playwright/test';
 test('it should display a music player', async ({ page }) => {
   await page.goto('/');
 
-const loggedUser =page.locator('.logged-user');
+  const loggedUser = page.locator('.logged-user');
 
   // Expect the music player to be visible.
   await expect(loggedUser).toHaveText('Fernando Papito');
+
+  await page.click('//div[contains(@class, "song")]//h6[text()="Bughium"]/..//button');
+
 });
