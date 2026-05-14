b52e029aec0a118a0146428856e84794b6ecabf4
Edson <edsonj82@outlook.com>
2026-05-14 20:33:01 -0300

    test: add music player rendering test with mocked api

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('it should display a music player', async ({ page }) => {
+test('it should validate user player', async ({ page }) => {
   const song = {
     id: 1,
     title: "Smell Like Test Script",
@@ -19,7 +19,30 @@ test('it should display a music player', async ({ page }) => {
   await page.goto('/');
 
   const loggedUser = page.locator('.logged-user');
-  await expect(loggedUser).toHaveText('Fernando Papito');  // Expect the music player to be visible.
+  await expect(loggedUser).toHaveText('Fernando Papito'); // Expect the music player to be visible.
+
+});
+
+test('it should display a music player', async ({ page }) => {
+  const song = {
+    id: 1,
+    title: "Smell Like Test Script",
+    artist: "Nullvana",
+    description: "Nullvana",
+    image: "https://raw.githubusercontent.com/qaxperience/mock/main/covers/nevertesting.jpg",
+    type: "album",
+    src: "https://raw.githubusercontent.com/qaxperience/mock/main/songs/nirvana.mp3"
+  }
+  await page.route('**/songs', route => route.fulfill({
+    status: 200,
+    contentType: 'application/json',
+    body: JSON.stringify([song])
+  }));
+
+  await page.goto('/');
+
+  // const loggedUser = page.locator('.logged-user');
+  // await expect(loggedUser).toHaveText('Fernando Papito');  
 
   const songCard = page.locator('.song')
     .filter({ hasText: song.title });
@@ -34,3 +57,8 @@ test('it should display a music player', async ({ page }) => {
   // await page.click('//div[contains(@class, "song")]//h6[text()="Bughium"]/..//button');
 
 });
+
+
+
+
+
