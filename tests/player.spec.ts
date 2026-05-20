adb54f9ae6fefbb6c60ac56f2b9ba365856b8cca
Edson <edsonj82@outlook.com>
2026-05-20 18:35:47 -0300

    refactor: introduce custom fixture for songs mock to eliminate duplication & fix: assert play button hidden state during music playback

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -1,19 +1,15 @@
 import { test, expect } from '@playwright/test';
+import songs from '../fixtures/songs.json';
+
 
 test('it should validate user player', async ({ page }) => {
-  const song = {
-    id: 1,
-    title: "Smell Like Test Script",
-    artist: "Nullvana",
-    description: "Nullvana",
-    image: "https://raw.githubusercontent.com/qaxperience/mock/main/covers/nevertesting.jpg",
-    type: "album",
-    src: "https://raw.githubusercontent.com/qaxperience/mock/main/songs/nirvana.mp3"
-  }
+
+  const MOCK_SONG = songs[0];
+
   await page.route('**/songs', route => route.fulfill({
     status: 200,
     contentType: 'application/json',
-    body: JSON.stringify([song])
+    body: JSON.stringify([MOCK_SONG])
   }));
 
   await page.goto('/');
@@ -24,19 +20,12 @@ test('it should validate user player', async ({ page }) => {
 });
 
 test('it should display a music player', async ({ page }) => {
-  const song = {
-    id: 1,
-    title: "Smell Like Test Script",
-    artist: "Nullvana",
-    description: "Nullvana",
-    image: "https://raw.githubusercontent.com/qaxperience/mock/main/covers/nevertesting.jpg",
-    type: "album",
-    src: "https://raw.githubusercontent.com/qaxperience/mock/main/songs/nirvana.mp3"
-  }
+  const MOCK_SONG = songs[0];
+
   await page.route('**/songs', route => route.fulfill({
     status: 200,
     contentType: 'application/json',
-    body: JSON.stringify([song])
+    body: JSON.stringify([MOCK_SONG])
   }));
 
   await page.goto('/');
@@ -45,15 +34,15 @@ test('it should display a music player', async ({ page }) => {
   // await expect(loggedUser).toHaveText('Fernando Papito');  
 
   const songCard = page.locator('.song')
-    .filter({ hasText: song.title });
+    .filter({ hasText: MOCK_SONG.title });
 
   const play = songCard.locator('.play')
   const pause = songCard.locator('.pause')
 
   await play.click();  // Click the play button on the song card.
   await expect(pause).toBeVisible();  // Expect the pause button to be visible after clicking play.
-  await expect(play).toBeVisible();  // Expect the play button to be visible again after some time (indicating the song has finished playing). 
-
+  // await expect(play).toBeVisible();  // Expect the play button to be visible again after some time (indicating the song has finished playing). 
+  await expect(play).toBeHidden();  // Expect the play button to be hidden after clicking play.
   // await page.click('//div[contains(@class, "song")]//h6[text()="Bughium"]/..//button');
 });
 
